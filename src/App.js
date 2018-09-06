42b03ba38192e27c3fd1bb89bb7ccb4c79dda042
Jean <djjean85@gmail.com>
2018-09-06 19:56:49 -0400

    added Main component, adding and displaying img works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Dropzone from "react-dropzone";
+import request from "superagent";
+import logo from "./logo.svg";
+import "./App.css";
+
+const CLOUDINARY_UPLOAD_PRESET = "fvsr5jia";
+const CLOUDINARY_UPLOAD_URL =
+  "https://api.cloudinary.com/v1_1/dub9ykyuq/upload";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      uploadedFileCloudinaryUrl: ""
+    };
+  }
+
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+
+  handleImageUpload(file) {
+    let upload = request
+      .post(CLOUDINARY_UPLOAD_URL)
+      .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
+      .field("file", file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== "") {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="FileUpload">
+          <Dropzone
+            multiple={false}
+            accept="image/*"
+            onDrop={this.onImageDrop.bind(this)}
+          >
+            <p>Drop an image or click to select a file to upload.</p>
+          </Dropzone>
+        </div>
+
+        <div>
+          {this.state.uploadedFileCloudinaryUrl === "" ? null : (
+            <div>
+              <p>{this.state.uploadedFile.name}</p>
+              <img src={this.state.uploadedFileCloudinaryUrl} />
+            </div>
+          )}
+        </div>
       </div>
     );
   }
