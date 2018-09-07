6d6582d72fba45f1ab361a0ebbb7d5122bf50b8f
Jean <djjean85@gmail.com>
2018-09-06 23:43:05 -0400

    lotta styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Dropzone from "react-dropzone";
 import request from "superagent";
 import ImageEditor from "./ImageEditor";
-import Nike from './Nike';
+import Nike from "./Nike";
 
 import "./App.css";
 
@@ -18,6 +18,8 @@ class App extends Component {
       uploadedFileCloudinaryUrl: "",
       topline: "",
       bottomline: "",
+      withBgColor: false,
+      position: ""
     };
   }
 
@@ -28,6 +30,13 @@ class App extends Component {
 
     this.handleImageUpload(files[0]);
   }
+
+  handleOnChange = evt => {
+    this.setState({
+      [evt.target.name]:
+        evt.target.type === "checkbox" ? evt.target.checked : evt.target.value
+    });
+  };
 
   handleImageUpload(file) {
     let upload = request
@@ -51,7 +60,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <link href="https://fonts.googleapis.com/css?family=Nunito:400,700,800,900" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css?family=Nunito:400,700,800,900"
+          rel="stylesheet"
+        />
+
+        <div className="nav">
+          <h1>Believe in Something Meme Generator</h1>
+          <p>Made By <a style={{color: "inherit"}} href="https://twitter.com/jeankvd">@jeankvd</a></p>
+        </div>
+
         <div className="FileUpload">
           <Dropzone
             multiple={false}
@@ -64,26 +82,88 @@ class App extends Component {
 
         <div>
           {this.state.uploadedFileCloudinaryUrl === "" ? (
-            <div>
-            <div
-              id="edit-img"
-              style={{
-                backgroundImage: `url('https://avatars0.githubusercontent.com/u/20847751?s=460&v=4')`
-              }}
-              alt=""
-            >
-              <div className="edit-text">
-                <h2>{this.state.topline}</h2>
-                <h2>{this.state.bottomline}</h2>
+            <div className="edit-container">
+              <div
+                id="edit-img"
+                style={{
+                  backgroundImage: `url('https://avatars0.githubusercontent.com/u/20847751?s=460&v=4')`
+                }}
+                alt=""
+              >
+                <div className="edit-text" style={{}}>
+                  <h2>
+                    <span
+                      style={{
+                        background: this.state.withBgColor ? "black" : "none"
+                      }}
+                    >
+                      {this.state.topline}
+                    </span>
+                  </h2>
+                  <h2>
+                    <span
+                      style={{
+                        background: this.state.withBgColor ? "black" : "none"
+                      }}
+                    >
+                      {this.state.bottomline}
+                    </span>
+                  </h2>
+                </div>
+                <div className="edit-logo">
+                  <h2
+                    style={{
+                      background: this.state.withBgColor ? "black" : "none"
+                    }}
+                  >
+                    Just Do It <Nike />
+                  </h2>
+                </div>
               </div>
-              <div className="edit-logo">
-                <h2>Just Do It <Nike/></h2>
+              <div className="inputs">
+                <h2>Edit The Text</h2>
+                <div className="input">
+                  <input
+                    type="text"
+                    name="topline"
+                    id="topline"
+                    onChange={this.handleOnChange}
+                    value={this.state.topline}
+                    placeholder="Top Line"
+                  />
+                </div>
+                <div className="input">
+                  <input
+                    type="text"
+                    name="bottomline"
+                    id="bottomline"
+                    onChange={this.handleOnChange}
+                    value={this.state.bottomline}
+                    placeholder="Bottom Line"
+                  />
+                </div>
+                <div className="input">
+                  <label htmlFor="checkbox">Dark Text Background</label>
+                  <input
+                    type="checkbox"
+                    name="withBgColor"
+                    id="withBgColor"
+                    onChange={this.handleOnChange}
+                    value={this.state.withBgColor}
+                  />
+                </div>
+                <a href="#">Download the Image</a>
+                {/* <select
+                  name="position"
+                  id="position"
+                  onChange={this.handleOnChange}
+                  value={this.state.position}
+                >
+                  <option value="Top">Top</option>
+                  <option value="Center">Center</option>
+                  <option value="Bottom">Bottom</option>
+                </select> */}
               </div>
-            </div>
-            <div className="inputs">
-              <input type="text" name="topline" id="topline" value={this.state.topline}/>
-              <input type="text" name="bottomline" id="bottomline" value={this.state.bottomline}/>
-            </div>
             </div>
           ) : (
             <ImageEditor
